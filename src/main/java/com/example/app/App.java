616db30fa446608d8b44ba5622f59a9d861d8716
Paweł Zabawa <pawel.zabawa.108wds@gmail.com>
2025-10-21 20:12:18 +0200

    Add initial Java project structure

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+/**
+ * Prosty program w języku Java, który wypisuje powitanie dla użytkowników
+ * repozytorium isapapi.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Witaj w nowym projekcie Java!");
+    }
+}
